1af1606a7cde070285cda8fd89d4db4bae30d600
Meghan Ventura <meghanventura@gmail.com>
2016-02-15 12:00:20 -0500

    Code reading - complete
    Finished code reading section. Broken code section not finished.

--- lesson_02_homework.rb
+++ lesson_02_homework.rb
@@ -1,19 +1,27 @@
 # Code Reading
 
 def fizzbuzz(num)
-  # Example comment line
+  # defines function fizzbuzz, accepting an integer "num"
 
   case
+    # case is like if / when statments, or switch case in C/C++. Prints the last statement that is true.
     when num % 15 == 0 then "FizzBuzz"
+      # if "num" is divisible by 15 with no remainder, Ruby prints FizzBuzz
     when num % 3  == 0 then "Fizz"
+      # if "num" is divisible by 3 with no remainder, Ruby prints FizzBuzz
     when num % 5  == 0 then "Buzz"
+      # if "num" is divisible by 5 with no remainder, Ruby prints FizzBuzz
     else num
+      # if "num" is not a modulus of 15, 3, or 5, Ruby prints the number
   end
 end
 
 def fizz_buzz_to(limit)
+  #defines function fizz_buzz_to, with an argument of "limit"
   1.upto(limit).each do |num|
+    # passing in integer values from 1 and up to + including "limit". (i.e., if limit is "8", it will pass values 1 - 8 through to do loop)
     puts fizzbuzz(num)
+      # puts 1 - "limit" through fizzbuzz function and prints the result
   end
 end
 
